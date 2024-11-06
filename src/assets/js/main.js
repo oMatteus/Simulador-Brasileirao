55432bff463a65d3d5bb9b8674267636f73ab2b8
oMatteus <88296251+oMatteus@users.noreply.github.com>
2024-11-05 22:41:52 -0300

    formatando os dados

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,6 +4,8 @@ async function getData(){
 
     try{
 
+        const tabela = []
+
         const response = await fetch('https://p1.trrsf.com/api/musa-soccer/ms-standings-games-light?idChampionship=1420&idPhase=&language=pt-BR&country=BR&nav=N&timezone=BR');
 
         if (!response.ok) {
@@ -14,19 +16,48 @@ async function getData(){
         const parser = new DOMParser();
         const doc = parser.parseFromString(texto, 'text/html');
 
-        const dados = doc.querySelector('ul.rounds');
-        ;
-        console.log(dados.querySelectorAll('li.round'));
+        const leitura = doc.querySelector('ul.rounds');
+        console.log(leitura);
 
-        // for(let dado in dados){
-        //     console.log(dado);
-        // }
+        //Encontra as rodadas
+        const rodadas = doc.querySelectorAll('li.round .header-round');
+        //Adiciona um objeto em cada indice do array
+        for(let rodada of rodadas){
+            tabela.push({
+                rodada: rodada.innerText
+            })
+        }
 
-        console.log(typeof(dados));
-        console.log(dados);
+        for(let i = 1; i < tabela.length+1; i++){
+            console.log(`#round-${i}`);
 
-        return dados
+            const rodadasData = doc.querySelectorAll(`#round-${i} .date-round`);
 
+            if (typeof tabela[i-1].datas !== 'object') {
+                tabela[i-1].datas = {}; // Inicializa como um objeto vazio, caso não seja
+            }
+
+            console.log(rodadasData);
+
+            rodadasData.forEach((value,key)=>{
+                console.log('================');
+                console.log(value.dataset.date);
+                console.log(key);
+
+                console.log(value.dataset.date);
+                tabela[i-1].datas[key] = {data: value.dataset.date};
+
+            })
+
+
+            // const a = 'aaaa'
+            // tabela[0].data = {a};
+
+            
+        }
+        
+        console.log(tabela);
+        return tabela
 
     }catch(e){
         console.error('Erro:', e);
